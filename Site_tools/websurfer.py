d3e2be73a17ad9f4718acf4f8e0e3f2057b92c7e
Henri Remonen <henri@remonen.fi>
2021-12-13 18:03:54 +0200

    Working on userinterface and menu features

--- Site_tools/websurfer.py
+++ Site_tools/websurfer.py
@@ -1,53 +1,73 @@
-from site_downloader import download_site
 import site_downloader
 import site_parser
+import queue
 
 class Instance:
     def __init__(self) -> None:
-        pass
+        self.queue = queue.Queue()
+        self.userInterface()
+
+    def userInterface(self):
+        CHOICES = ["s", "d", "p", "e"]
+        choices = """
+        Choices: 
+        (s)ettings
+        (d)ownload site
+        (p)arse site and create dataset
+        (e)xit
+        """
+
+        print("""
+    Welcome to WebSurfer!
+    What would you like to do?""")
+
+        while True:
+            print("Main menu.")
+            print(choices)
+            c = input("Enter choice: ")
+            if c.lower() == "e":
+                break
+            elif c.lower() not in CHOICES:
+                print("Incorrect choice, try again")
+            elif c == "s":
+                self.__settingsMenu()
+            elif c == "d":
+                self.__downloadMenu()           
+            elif c == "p":
+                dataset = self.__parseMenu()
+                print(dataset)
+            else:
+                print("Something went wrong.")
 
 
-def userInterface():
-    CHOICES = ["s", "d", "p", "e"]
-    choices = """
-Choices: 
-(s)ettings
-(d)ownload site
-(p)arse site and create dataset
-(e)xit
-    """
+    def __settingsMenu(self):
+        SETTINGS_CHOICES = ["c", "i", "e"]
+        settings_choices = """
+        (c)reate queue
+        (i)mport queue
+        (e)xit
+        """
+         
+        while True:
+            print("Settings menu.")
+            print(settings_choices)
+            c = input("Enter choice: ")
+            if c.lower() == "e":
+                break
+        
 
-    print("""Welcome to WebSurfer!
-What would you like to do?""")
-
-    while True:
-        print(choices)
-        c = input("Enter choice: ")
-        if c.lower() == "e":
-            break
-        elif c.lower() not in CHOICES:
-            print("Incorrect choice, try again")
-        else: 
-            try:
-                action(c)
-            except Exception:
-                print("Someting went wrong, try again.")
-
-def action(choice: str):
-    if choice == "s":
-        print("Settings menu.")
-
-    elif choice == "d":
+    def __downloadMenu(self):
         print("Download menu.")
         site_to_download = input("Enter site URL: ")
         print()
-        site_downloader.download_site(site_to_download)
-        
-    elif choice == "p":
-        print("Parse menu.")
 
-#p = site_parser.Parser()
-#data = p.create_dataset()
+        site_downloader.download_site(site_to_download)
+
+    def __parseMenu(self):
+        print("Parse menu.")
+        p = site_parser.Parser()
+        return p.create_dataset()
+
 
 #You could start downloading all the associated URLs related to
 #the initial sites URL
@@ -57,4 +77,4 @@ def action(choice: str):
 #You could also implement db or something else to store
 #the downloaded sites.
 if __name__ == "__main__":
-    userInterface()
+    Instance()
