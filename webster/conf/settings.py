4c7cc4ca03e1b65a9ac559f735badcf44dcc013f
Henri Remonen <henri@remonen.fi>
2022-06-25 11:33:50 +0300

    Created settings file for project

---
+++ webster/conf/settings.py
@@ -0,0 +1,19 @@
+import os
+
+from datetime import datetime
+
+"""
+Project settings.
+"""
+
+###########################--SETTINGS--###########################
+
+#===================---PATH SETTINGS---===================
+TIME_NOW = datetime.now()
+PATH = os.path
+
+#Path to downloads folder
+DL_DIR = "downloads/html/"
+PARSE_DIR = "downloads/json/"
+
+###########################-/SETTINGS/-###########################
